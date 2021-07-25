5255968882144152e2726627d41f5c11846a3ade
Magdiel3 <magdieltercero@hotmail.com>
2021-07-25 12:28:30 -0500

    Added image upload to script

--- save_image.sh
+++ save_image.sh
@@ -4,3 +4,9 @@ docker build . -t autoreply-gpi-bot
 # Compress image
 docker image save autoreply-gpi-bot:latest -o autoreply-gpi-bot.tar
 gzip autoreply-gpi-bot.tar
+
+# Copy image to EC2 instance
+scp -i "gpi-bot-instance.pem" autoreply-gpi-bot.tar.gz ubuntu@ec2-18-224-29-202.us-east-2.compute.amazonaws.com:/tmp
+
+# Connect to instance
+ssh -i "gpi-bot-instance.pem" ubuntu@ec2-18-224-29-202.us-east-2.compute.amazonaws.com
